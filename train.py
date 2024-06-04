1a5b17d64879e21c553d98387ea2a4002d278b7f
Leo Chen <yuliangchen@ucsb.edu>
2024-06-04 12:07:32 -0700

    init

--- train.py
+++ train.py
@@ -75,3 +75,8 @@ if __name__ == '__main__':
             model.save_networks(epoch)
 
         print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
+
+
+
+
+#python train.py --dataroot pannels_train_test --name maps_cyclegan --model cycle_gan
